28103acf3f66b586db2587dcd076c93c17c57dcc
adympt <adymptgaming@gmail.com>
2024-12-10 17:06:58 -0800

    updated scripts for eb

---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from app import application  # Import the Flask instance 'application' from app/__init__.py
+
+if __name__ == "__main__":
+    application.run()  # Use 'application' instead of 'app'
