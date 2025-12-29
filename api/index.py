da570be79cc3be4de191ae7a626d7651c470dca0
GeorgeTCecil <georgetcecil@gmail.com>
2025-12-29 09:58:34 -0800

    Add Vercel serverless handler pattern

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'StayScout Test - Flask is working!'
-
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return f'Path: {path}'
+    return f'''
+    <h1>StayScout Test</h1>
+    <p>Flask is working!</p>
+    <p>Path: /{path}</p>
+    <p>Request method: {request.method}</p>
+    '''
+
+# Vercel serverless handler
+def handler(event, context):
+    return app(event, context)
