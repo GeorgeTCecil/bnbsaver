306043f5bfc2178e3809fdfbb2860058cdb08639
GeorgeTCecil <georgetcecil@gmail.com>
2025-12-29 09:55:57 -0800

    Minimal Flask test

--- api/index.py
+++ api/index.py
@@ -1,45 +1,11 @@
-"""
-StayScout - Vercel Serverless Entry Point
-"""
-from flask import Flask, jsonify
-import sys
-import os
+from flask import Flask
 
-# Simple test app to verify deployment works
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({
-        'status': 'alive',
-        'message': 'StayScout test deployment',
-        'python_version': sys.version,
-        'cwd': os.getcwd(),
-        'files': os.listdir('.')[:20]
-    })
+    return 'StayScout Test - Flask is working!'
 
-@app.route('/test-import')
-def test_import():
-    errors = []
-    imports = {}
-
-    # Test each import
-    try:
-        import demo_results
-        imports['demo_results'] = 'OK'
-    except Exception as e:
-        imports['demo_results'] = str(e)
-        errors.append(f'demo_results: {e}')
-
-    try:
-        from application import app as main_app
-        imports['application'] = 'OK'
-    except Exception as e:
-        imports['application'] = str(e)
-        errors.append(f'application: {e}')
-
-    return jsonify({
-        'imports': imports,
-        'errors': errors,
-        'sys_path': sys.path[:5]
-    })
+@app.route('/<path:path>')
+def catch_all(path):
+    return f'Path: {path}'
